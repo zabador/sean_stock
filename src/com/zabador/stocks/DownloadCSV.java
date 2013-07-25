fc9ce6e09c150f5b15c50defd25a7f812ea39ccf
Skye <snschnei@oakland.edu>
2013-07-25 11:30:43 -0400

    added header to csv file

--- src/com/zabador/stocks/DownloadCSV.java
+++ src/com/zabador/stocks/DownloadCSV.java
@@ -2,6 +2,7 @@ package com.zabador.stocks;
 
 import java.io.IOException;
 import java.io.FileOutputStream;
+import java.io.RandomAccessFile;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
@@ -9,20 +10,37 @@ import java.nio.channels.ReadableByteChannel;
 public class DownloadCSV {
 
     private String searchTerms;
+    private String header;
 
-    public DownloadCSV(String searchTerms) {
+    public DownloadCSV(String header, String searchTerms) {
 
         this.searchTerms = searchTerms;
+        this.header = header;
     }
 
     public void download() {
         try {
-            URL website = new URL("http://download.finance.yahoo.com/d/quotes.csv?s=GOOG&f="+ searchTerms +"&e=.csv");
+            URL website = new URL("http://download.finance.yahoo.com/d/quotes.csv?s=GOOG+AAPL&f="+ searchTerms +"&e=.csv");
             ReadableByteChannel rbc = Channels.newChannel(website.openStream());
             FileOutputStream fos = new FileOutputStream("stockInfo.csv");
             fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+            prepend();
         }catch(IOException ioe) {
             System.out.println(ioe.getMessage());
+        }
+    }
+
+    public void prepend() {
+        try {
+            RandomAccessFile file = new RandomAccessFile("stockInfo.csv","rws");
+            byte[] text = new byte[(int) file.length()];
+            file.readFully(text);
+            file.seek(0);
+            file.writeBytes(header);
+            file.write(text);
+            file.close();
+        }catch(Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
--- src/com/zabador/stocks/MainView.java
+++ src/com/zabador/stocks/MainView.java
@@ -7,9 +7,11 @@ import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class MainView implements ActionListener {
@@ -17,20 +19,12 @@ public class MainView implements ActionListener {
     private Map<String, String> map;
     private ArrayList<JCheckBox> checkboxes = new ArrayList<JCheckBox>();
     private JButton submit;
+    private JFrame frame;
 
     public MainView() {
         fillMap();
         mapCheckboxes();
         init();
-    }
-
-    public void fillMap() {
-        map = new HashMap<String, String>();
-        map.put("Symbol", "s");
-        map.put("Name", "n");
-        map.put("Last Trade (Price Only)", "l1");
-        map.put("Change in Percent", "p2");
-        
     }
 
     public void mapCheckboxes() {
@@ -41,10 +35,11 @@ public class MainView implements ActionListener {
     }
 
     public void init() {
-        JFrame frame = new JFrame("Stock Info Selector");
+        frame = new JFrame("Stock Info Selector");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,200);
-        frame.setLayout(new GridLayout());
+        frame.setLayout(new GridLayout(0,10));
+
         for(JCheckBox cb : checkboxes) {
             frame.getContentPane().add(cb);
         }
@@ -60,14 +55,106 @@ public class MainView implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == submit) {
             String searchTerms = "";
+            String header = "";
+
             for(JCheckBox cb : checkboxes) {
                 if(cb.isSelected()) {
                     String key = cb.getText();
+                    header += key+",";
                     searchTerms += map.get(key);
                 }
             }
-            DownloadCSV download = new DownloadCSV(searchTerms);
+
+            header = header.replaceAll(",$","");
+            header += "\n";
+            DownloadCSV download = new DownloadCSV(header, searchTerms);
             download.download();
+            JOptionPane.showMessageDialog(frame, "File finished downloading");
         }
     }
+
+    public void fillMap() {
+        map = Collections.synchronizedMap(new LinkedHashMap<String, String>());
+        map.put("Symbol","s");
+        map.put("Name","n");
+        map.put("Last Trade (With Time)","l");
+        map.put("Last Trade (Price Only)","l1");
+        map.put("Last Trade Date","d1");
+        map.put("Last Trade Time","t1");
+        map.put("Last Trade Size","k3");
+        map.put("Change and Percent Change","c");
+        map.put("Change","c1");
+        map.put("Change in Percent","p2");
+        map.put("Ticker Trend","t7");
+        map.put("Volume","v");
+        map.put("Average Daily Volume","a2");
+        map.put("More Info","i");
+        map.put("Trade Links","t6");
+        map.put("Bid","b");
+        map.put("Bid Size","b6");
+        map.put("Ask","a");
+        map.put("Ask Size","a5");
+        map.put("Previous Close","p");
+        map.put("Open","o");
+        map.put("Day's Range","m");
+        map.put("52-week Range","w");
+        map.put("Change From 52-wk Low","j5");
+        map.put("Pct Chg From 52-wk Low","j6");
+        map.put("Change From 52-wk High","k4");
+        map.put("Pct Chg From 52-wk High","k5");
+        map.put("Earnings/Share","e");
+        map.put("P/E Ratio","r");
+        map.put("Short Ratio","s7");
+        map.put("Dividend Pay Date","r1");
+        map.put("Ex-Dividend Date","q");
+        map.put("Dividend/Share","d");
+        map.put("Dividend Yield","y");
+        map.put("Float Shares","f6");
+        map.put("Market Capitalization","j1");
+        map.put("1yr Target Price","t8");
+        map.put("EPS Est. Current Yr","e7");
+        map.put("EPS Est. Next Year","e8");
+        map.put("EPS Est. Next Quarter","e9");
+        map.put("Price/EPS Est. Current Yr","r6");
+        map.put("Price/EPS Est. Next Yr","r7");
+        map.put("PEG Ratio","r5");
+        map.put("Book Value","b4");
+        map.put("Price/Book","p6");
+        map.put("Price/Sales","p5");
+        map.put("EBITDA","j4");
+        map.put("50-day Moving Avg","m3");
+        map.put("Change From 50-day Moving Avg","m7");
+        map.put("Pct Chg From 50-day Moving Avg","m8");
+        map.put("200-day Moving Avg","m4");
+        map.put("Change From 200-day Moving Avg","m5");
+        map.put("Pct Chg From 200-day Moving Avg","m6");
+        map.put("Shares Owned","s1");
+        map.put("Price Paid","p1");
+        map.put("Commission","c3");
+        map.put("Holdings Value","v1");
+        map.put("Day's Value Change","w1,");
+        map.put("Holdings Gain Percent","g1");
+        map.put("Holdings Gain","g4");
+        map.put("Trade Date","d2");
+        map.put("Annualized Gain","g3");
+        map.put("High Limit","l2");
+        map.put("Low Limit","l3");
+        map.put("Notes","n4");
+        map.put("Last Trade (Real-time) with Time","k1");
+        map.put("Bid (Real-time)","b3");
+        map.put("Ask (Real-time)","b2");
+        map.put("Change Percent (Real-time)","k2");
+        map.put("Change (Real-time)","c6");
+        map.put("Holdings Value (Real-time)","v7");
+        map.put("Day's Value Change (Real-time)","w4");
+        map.put("Holdings Gain Pct (Real-time)","g5");
+        map.put("Holdings Gain (Real-time)","g6");
+        map.put("Day's Range (Real-time)","m2");
+        map.put("Market Cap (Real-time)","j3");
+        map.put("P/E (Real-time)","r2");
+        map.put("After Hours Change (Real-time)","c8");
+        map.put("Order Book (Real-time)","i5");
+        map.put("Stock Exchange","x");
+    }
+
 }
