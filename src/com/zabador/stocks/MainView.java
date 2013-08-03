cf59a4b58f0f32fd576e5eb30dd2cad24e627a33
Zabador <zabador2000@gmail.com>
2013-08-02 23:04:12 -0400

    removed some code

--- src/com/zabador/stocks/MainView.java
+++ src/com/zabador/stocks/MainView.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import java.awt.*;
 
 import javax.swing.*;
+import javax.swing.border.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -48,29 +49,29 @@ public class MainView implements ActionListener {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         listModel = new DefaultListModel();
         searchModel = new DefaultListModel();
-        userInputPanel = new javax.swing.JPanel();
-        enterSymbolLbl = new javax.swing.JLabel();
-        orLbl = new javax.swing.JLabel();
-        searchLbl = new javax.swing.JLabel();
-        symbolTxt = new javax.swing.JTextField();
-        searchTxt = new javax.swing.JTextField();
-        add = new javax.swing.JButton();
-        searchBtn = new javax.swing.JButton();
-        jScrollList = new javax.swing.JScrollPane();
-        searchList = new javax.swing.JList();
-        jScrollPanel = new javax.swing.JScrollPane();
-        checkboxPanel = new javax.swing.JPanel();
-        symbolPane = new javax.swing.JScrollPane();
-        symbols = new javax.swing.JList();
-        remove = new javax.swing.JButton();
-        submit = new javax.swing.JButton();
+        userInputPanel = new JPanel();
+        enterSymbolLbl = new JLabel();
+        orLbl = new JLabel();
+        searchLbl = new JLabel();
+        symbolTxt = new JTextField();
+        searchTxt = new JTextField();
+        add = new JButton();
+        searchBtn = new JButton();
+        jScrollList = new JScrollPane();
+        searchList = new JList();
+        jScrollPanel = new JScrollPane();
+        checkboxPanel = new JPanel();
+        symbolPane = new JScrollPane();
+        symbols = new JList();
+        remove = new JButton();
+        submit = new JButton();
         addSearchBtn = new JButton();
 
         jScrollPanel.getVerticalScrollBar().setUnitIncrement(16);
 
-        frame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        userInputPanel.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
+        userInputPanel.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
 
         enterSymbolLbl.setText("Enter Company's Symbol:");
 
@@ -82,31 +83,31 @@ public class MainView implements ActionListener {
 
         searchBtn.setText("Search");
 
-        searchList.setBackground(new java.awt.Color(238, 238, 238));
-        searchList.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createEtchedBorder(), "Search Results"));
+        searchList.setBackground(new Color(238, 238, 238));
+        searchList.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Search Results"));
         searchList.setModel(searchModel);
 
         jScrollList.setViewportView(searchList);
 
         addSearchBtn.setText("Add -->");
 
-        javax.swing.GroupLayout userInputPanelLayout = new javax.swing.GroupLayout(userInputPanel);
+        GroupLayout userInputPanelLayout = new GroupLayout(userInputPanel);
         userInputPanel.setLayout(userInputPanelLayout);
         userInputPanelLayout.setHorizontalGroup(
-                userInputPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                userInputPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                 .addGroup(userInputPanelLayout.createSequentialGroup()
                     .addContainerGap()
-                    .addGroup(userInputPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(userInputPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addGroup(userInputPanelLayout.createSequentialGroup()
-                            .addGroup(userInputPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(userInputPanelLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
                                 .addGroup(userInputPanelLayout.createSequentialGroup()
                                     .addComponent(searchLbl)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                    .addGroup(userInputPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                        .addComponent(jScrollList, javax.swing.GroupLayout.DEFAULT_SIZE, 189, Short.MAX_VALUE)
+                                    .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                                    .addGroup(userInputPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(jScrollList, GroupLayout.DEFAULT_SIZE, 189, Short.MAX_VALUE)
                                         .addComponent(searchTxt))
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addGroup(userInputPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addGroup(userInputPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                                         .addComponent(searchBtn)
                                         .addComponent(addSearchBtn)))
                                 .addGroup(userInputPanelLayout.createSequentialGroup()
@@ -115,51 +116,51 @@ public class MainView implements ActionListener {
                                     .addGap(2, 2, 2)))
                             .addGap(16, 16, 16))
                         .addGroup(userInputPanelLayout.createSequentialGroup()
-                            .addGroup(userInputPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(userInputPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                                 .addComponent(orLbl)
                                 .addGroup(userInputPanelLayout.createSequentialGroup()
                                     .addComponent(enterSymbolLbl)
                                     .addGap(63, 63, 63)
-                                    .addComponent(symbolTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                            .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                                    .addComponent(symbolTxt, GroupLayout.PREFERRED_SIZE, 189, GroupLayout.PREFERRED_SIZE)))
+                            .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                             );
         userInputPanelLayout.setVerticalGroup(
-                userInputPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                userInputPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                 .addGroup(userInputPanelLayout.createSequentialGroup()
                     .addContainerGap()
-                    .addGroup(userInputPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addGroup(userInputPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                         .addComponent(enterSymbolLbl)
-                        .addComponent(symbolTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(symbolTxt, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                         .addComponent(add))
-                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
                     .addComponent(orLbl)
                     .addGap(18, 18, 18)
-                    .addGroup(userInputPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addGroup(userInputPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                         .addComponent(searchLbl)
-                        .addComponent(searchTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(searchTxt, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                         .addComponent(searchBtn))
-                    .addGroup(userInputPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(userInputPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                         .addGroup(userInputPanelLayout.createSequentialGroup()
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                            .addComponent(jScrollList, javax.swing.GroupLayout.DEFAULT_SIZE, 173, Short.MAX_VALUE)
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(jScrollList, GroupLayout.DEFAULT_SIZE, 173, Short.MAX_VALUE)
                             .addContainerGap())
                         .addGroup(userInputPanelLayout.createSequentialGroup()
                             .addGap(81, 81, 81)
                             .addComponent(addSearchBtn)
-                            .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                            .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                             );
 
 
-        jScrollPanel.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED), "Select Your Options"));
+        jScrollPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED), "Select Your Options"));
 
-        javax.swing.GroupLayout checkboxPanelLayout = new javax.swing.GroupLayout(checkboxPanel);
+        GroupLayout checkboxPanelLayout = new GroupLayout(checkboxPanel);
         checkboxPanel.setLayout(checkboxPanelLayout);
         checkboxPanelLayout.setHorizontalGroup(
-                checkboxPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                checkboxPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                 .addGap(0, 623, Short.MAX_VALUE)
                 );
         checkboxPanelLayout.setVerticalGroup(
-                checkboxPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                checkboxPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                 .addGap(0, 343, Short.MAX_VALUE)
                 );
 
@@ -175,7 +176,7 @@ public class MainView implements ActionListener {
 
         jScrollPanel.setViewportView(checkboxPanel);
 
-        symbols.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createEtchedBorder(), "Symbols"));
+        symbols.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Symbols"));
         symbols.setModel(listModel);
         symbolPane.setViewportView(symbols);
 
@@ -183,40 +184,40 @@ public class MainView implements ActionListener {
 
         submit.setText("Submit");
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(frame.getContentPane());
+        GroupLayout layout = new GroupLayout(frame.getContentPane());
         frame.getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
                 .addGroup(layout.createSequentialGroup()
                     .addContainerGap()
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                        .addComponent(jScrollPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
-                        .addComponent(userInputPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(jScrollPanel, GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                        .addComponent(userInputPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGap(9, 9, 9)
                     .addComponent(remove)
-                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                    .addComponent(symbolPane, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 24, Short.MAX_VALUE)
+                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                    .addComponent(symbolPane, GroupLayout.PREFERRED_SIZE, 108, GroupLayout.PREFERRED_SIZE)
+                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 24, Short.MAX_VALUE)
                     .addComponent(submit)
                     .addContainerGap())
                 );
         layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                     .addContainerGap()
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
                         .addGroup(layout.createSequentialGroup()
-                            .addComponent(jScrollPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 290, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jScrollPanel, GroupLayout.PREFERRED_SIZE, 290, GroupLayout.PREFERRED_SIZE)
                             .addGap(18, 18, 18)
-                            .addComponent(userInputPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(userInputPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGroup(layout.createSequentialGroup()
                             .addGap(0, 0, Short.MAX_VALUE)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
                                 .addComponent(submit)
                                 .addGroup(layout.createSequentialGroup()
                                     .addComponent(remove)
                                     .addGap(187, 187, 187))
-                                .addComponent(symbolPane, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                .addComponent(symbolPane, GroupLayout.PREFERRED_SIZE, 212, GroupLayout.PREFERRED_SIZE))))
                     .addContainerGap())
                 );
 
